2ea8068d4acf81e575562ece595d6359f0449af0
ChiefLopes <chieflopesekede@gmail.com>
2023-03-24 12:40:17 +0100

    added javascript

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 
-// console.log(document.querySelector(".guess").value);    
+// console.log(document.querySelector(".guess").value);   
+
+const func = function () {
+    console.log('You just clicked CHECK!');
+}
 
 const checkBtn = document.querySelector('.check');
 checkBtn.addEventListener('click', () => {
-        
+    console.log(document.querySelector(".guess").value);
+
+    console.log(document.querySelector('.message').textContent = 'correct number!');  
+    
+    console.log('Let"s go again');
 })
