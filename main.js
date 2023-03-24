6eeba6de45f501284af4c92689045939751eea3a
ChiefLopes <chieflopesekede@gmail.com>
2023-03-24 12:08:15 +0100

    initial commit of my number guessing app

---
+++ main.js
@@ -0,0 +1,9 @@
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector(".guess").value);    
+
+const checkBtn = document.querySelector('.check');
+checkBtn.addEventListener('click', () => {
+        
+})
