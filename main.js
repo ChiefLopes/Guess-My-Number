6416d297ec597b44a7a043dd5b97e3623b264720
ChiefLopes <chieflopesekede@gmail.com>
2023-03-24 15:52:20 +0100

    added more features with javascript

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 // document.querySelector('.score').textContent = 10;
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber    ;
 
+let score = 20;
+let highscore = 0;
 
 const checkBtn = document.querySelector('.check');
 checkBtn.addEventListener('click', () => {
@@ -11,7 +13,28 @@ checkBtn.addEventListener('click', () => {
     console.log(guess, typeof guess);
 
     if (!guess) {
-        document.querySelector(".message").textContent = 'No number entered!';
-        
+      document.querySelector(".message").textContent = "No number entered!";
+    } else if (guess < secretNumber) {
+        document.querySelector(".message").textContent = "Too low!";
+         score --; 
+        document.querySelector(".score").textContent = score;
+    } else if (guess > secretNumber) {
+        document.querySelector(".message").textContent = "Too high!";
+        score --;
+        document.querySelector(".score").textContent = score;
     }
+    else if (guess !== secretNumber) {
+      document.querySelector(
+        ".message"
+      ).textContent = `Wrong Number! Try again...`;
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "Correct!";
+        score++;
+        document.querySelector(".score").textContent = score;  
+        highscore = ;
+        document.querySelector(".highscore").textContent = highscore;
+    
+    }
+
 })
+
