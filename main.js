52b27e8060825ccbd33cd08d6c2c9a5568a8e0f9
ChiefLopes <chieflopesekede@gmail.com>
2023-03-25 18:37:59 +0100

    refreshed my javascript codes

--- main.js
+++ main.js
@@ -1,46 +1,14 @@
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
 
-
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber    ;
-
-let score = document.querySelector('.score').textContent = 5;
-let highscore = 0;
-
-const checkBtn = document.querySelector('.check');
-checkBtn.addEventListener('click', () => {
-    const guess = Number(document.querySelector(".guess").value)
+document.querySelector(".check").addEventListener(
+  "click", () => {
+    const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
 
+// code here prints message "No number entered" ~
     if (!guess) {
-      document.querySelector(".message").textContent = "No number entered!";
-    } else if (guess < secretNumber) {
-        document.querySelector(".message").textContent = "Too low!";
-         score --; 
-        document.querySelector(".score").textContent = score;
-    } else if (guess > secretNumber) {
-      if (score > 0) {
-           document.querySelector(".message").textContent = "Too high!";
-           score--;
-           document.querySelector(".score").textContent = score;
-      }
-       
+      document.querySelector(".message").textContent = "No number entered!"
     }
-    else if (guess !== secretNumber) {
-      document.querySelector(
-        ".message"
-      ).textContent = `Wrong Number! Try again...`;
-    } else if (guess === secretNumber) {
-        document.querySelector(".message").textContent = "Correct!";
-        score++;
-        document.querySelector(".score").textContent = score;  
-      highscore = score++;
-        document.querySelector(".highscore").textContent = highscore;
-    
-    }
-  
-
-})
-
-
-
+  }
+)
